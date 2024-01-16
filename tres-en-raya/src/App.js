b53f63e5770488fea3195f1d3bcea91326951b7c
wendyEQQ <153571873+wendyEQQ@users.noreply.github.com>
2024-01-16 14:12:16 +0000

    ryudgh

---
+++ tres-en-raya/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Tablero from './components/Tablero';
+import TableroB from './components/TableroB';
+
+function App() {
+  return (
+    <div className="App">
+      <Tablero/>
+    
+      
+        
+    </div>
+  );
+}
+
+export default App;
