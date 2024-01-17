db11968bf390ff68f9bff9e0d9469a440e345afd
wendyEQQ <153571873+wendyEQQ@users.noreply.github.com>
2024-01-17 13:06:52 +0000

    rdytdr

--- tres-en-raya/src/App.js
+++ tres-en-raya/src/App.js
@@ -1,14 +1,25 @@
+import React from 'react';
 import './App.css';
 import Tablero from './components/Tablero';
 import TableroB from './components/TableroB';
+import TableroC from './components/TableroC';
+import TableroD from './components/TableroD';
+import TableroE from './components/TableroE';
+import Juego from './components/Juego';
+import JuegoA from './components/JuegoA';
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <Tablero/>
-    
-      
-        
+
+      {/* <Tablero />
+      <TableroB />
+      <TableroE />
+      <Juego />*/}
+      <JuegoA />
     </div>
   );
 }
